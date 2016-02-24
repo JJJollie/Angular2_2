2ceccf1aca71b1c86739bbbe1ac4a7d9f5e65c5b
Youna <JJJollie@users.noreply.github.com>
2016-02-24 19:41:51 +1300

    Added files via upload

---
+++ app/app.js
@@ -0,0 +1,9 @@
+var App = angular.module('App', []);
+
+App.controller('TodoCtrl', function($scope, $http) {
+  $http.get('todos.json')
+       .then(function(res){
+          $scope.todos = res.data;                
+        });
+});
+// JavaScript Document
